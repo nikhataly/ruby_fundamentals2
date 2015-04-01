220ead8b2b54fd536102458df129857d82ff4750
NIKHAT BHAYANI <NIKHATALY@GMAIL.COM>
2015-04-01 15:54:32 -0400

    exercise 6 not has an alphabetically  sorted list instead of a regular list

--- exercise6.rb
+++ exercise6.rb
@@ -6,9 +6,11 @@ def printlist (grocery_list)
     grocery_list.each {|x| puts "* #{x}"}
 end
 
-printlist(grocery_list)
+printlist(grocery_list.sort)
+
 
 if grocery_list.include?("banana")
     print "You need to pick up bananas"
 else print "You don't need to pick up bananas today"
 end
+
