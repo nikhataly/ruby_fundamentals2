861e4ec082c2ceb576b8ba43f5675713a7f02c55
NIKHAT BHAYANI <NIKHATALY@GMAIL.COM>
2015-04-01 15:41:57 -0400

    Commit exercise 6, including method and checking for bananas

--- exercise6.rb
+++ exercise6.rb
@@ -1,8 +1,14 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-grocery_list.each {|x| puts "* #{x}"}
-
 grocery_list.push("rice")
 
-puts grocery_list
+def printlist (grocery_list)
+    grocery_list.each {|x| puts "* #{x}"}
+end
 
+printlist(grocery_list)
+
+if grocery_list.include?("banana")
+    print "You need to pick up bananas"
+else print "You don't need to pick up bananas today"
+end
