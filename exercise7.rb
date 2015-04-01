d8c47c5594591ab41cb04a239a104279780bf149
NIKHAT BHAYANI <NIKHATALY@GMAIL.COM>
2015-04-01 17:25:08 -0400

    Exercise7 completed

--- exercise7.rb
+++ exercise7.rb
@@ -12,7 +12,23 @@ display(students)
 
 students[:cohort4] = 43
 
+# puts students.inspect
+
+
+students.each do |key, value|
+    "puts #{key}: #{value.to_f*1.05}"
+end
+
+
+students.delete(:cohort4)
+
 puts students.inspect
 
+total_students = 0
+students.values.each do |value|
+    total_students += value
+end
 
-students.each_value{|key| puts key}
+ puts total_students
+
+
